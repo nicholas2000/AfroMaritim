775f5d6a1a948397b58948e431f60b0ca707ff70
nicholas2000 <sughapur@gmail.com>
2023-01-31 15:54:19 +0700

    isengaja

--- resources/views/sidebar/dashboard.blade.php
+++ resources/views/sidebar/dashboard.blade.php
@@ -91,38 +91,6 @@
                         Welcome Admin
                     </div>
                     <div class='card-body'>
-                        
-<div class="container">
-	<div class="row">
-		<div class="col-12 mt-4 text-center">
-			<p><h1 class="font-weight-bold">HOW TO</h1></p>
-			<p><h2 class="mb-5">Vertically center the Bootstrap 4 modal popin</h2></p>
-			<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#exampleModal">Open the Booty modal</button>
-		</div>
-	</div>
-</div>
-
-<!-- Modal -->
-<div class="modal modal-vcenter fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Penting!</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-				<p class="mb-0"><strong>Yakin Update Data? </strong></p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tidak</button>
-        <button type="button" class="btn btn-primary" data-dismiss="modal">Iya</button>
-      </div>
-    </div>
-  </div>
-</div>
-
     
   </div>
 
