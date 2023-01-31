e6307100ec5c69cbf22a98b7de287aa810dad5c0
NicholasTj <64400543+NicholasTj@users.noreply.github.com>
2023-01-31 13:55:16 +0700

    Perbaikan History Transaksi

--- resources/views/form/FormTransaksi.blade.php
+++ resources/views/form/FormTransaksi.blade.php
@@ -2,146 +2,166 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
 <section class="order-form m-4">
     <div class="container pt-4">
         <div class="container">
             <div class="form-group">
-                <div class="col-md-3 inputGroupContainer">
-                    <div class="input-group">
+                <div class="container">
+                    <div class="row">
+                      <div class="col-sm>
                         <span class="input-group-addon"><i class="glyphicon glyphicon-barcode"></i></span>
                         <input name="" placeholder="Cari Halaman" class="form-control"  type="text" >
+                      </div>
+                      <div class="col-5"></div>
+                      <div class="" style="">
+                        Nama Admin :
+                      </div>
                     </div>
-                </div>
+                  </div>
             </div>
             <br>
-            <div class="col-12 ">
+            <div class="col-12">
                 <h2> Penjualan</h2>
                 <br>
             </div>
-            <br>
-
-                {{-- ----- --}}
-
             </div>
 
+            <div class="container">
+                <div class="row">
+                  <div class="col-12">
+                    <h3> Customer</h3>
+                  </div>
+                </div>
+              </div>
+
+              <div class="container">
+                <div class="row">
+                    <div class="col-sm-1">
+
+                    </div>
+                  <div class="col-sm-3">
+                     Kode Transaksi :
+                  </div>
+                  <div class="col-sm-3">
+                    <input  name="kode" class="form-control" type="text" disabled>
+                  </div>
+                  <div class="col-sm">
+                    Tanggal :
+                  </div>
+                  <div class="col-sm-3">
+
+                  </div>
+                </div>
+              </div>
+              <div class="container">
+                <div class="row">
+                    <div class="col-sm-1">
+
+                    </div>
+                  <div class="col-sm-3">
+                     Admin :
+                  </div>
+                  <div class="col-sm-3">
+
+                  </div>
+                  <div class="col-sm">
+                    Waktu :
+                  </div>
+                  <div class="col-sm-3">
+
+                  </div>
+                </div>
+              </div>
+              <br>
+              <div class="container">
+                <div class="row">
+                    <div class="col-sm-1">
+
+                    </div>
+                  <div class="col-sm-3">
+                     Subtotal :
+                  </div>
+                  <div class="col-sm-3">
+
+                  </div>
+                </div>
+              </div>
+              <br>
+              <div class="container">
+                <div class="row">
+                    <div class="col-sm-1">
+
+                    </div>
+                  <div class="col-sm-6">
+                     Pilih Diskon :
+                  </div>
+                  <div class="col-sm-3">
+                    <button type="button" style="float: right;" name="" class="btn btn-outline-danger">Bayar</button>
+                  </div>
+                </div>
+              </div>
+              <br>
+              <br>
+              <br>
+
+              <div class="container">
+                <div class="row">
+                  <div class="col-sm-4">
+                     Daftar Pesanan :
+                  </div>
+                  <div class="col-sm-3">
+                    Ukuran :
+                  </div>
+                  <div class="col-sm-3">
+                    Volume :
+                  </div>
+                  <div class="col-sm-2">
+                    Berat :
+                  </div>
+                </div>
+              </div>
+              <br>
+              <br>
+              <br>
+              <div class="container">
+                <div class="row">
+                  <div class="col-sm-4">
+                     Transaksi :
+                  </div>
+                </div>
+              </div>
+              <br>
+              <div class="container">
+                <div class="row">
+                  <div class="col-sm">
+                     Nama Customer
+                  </div>
+                  <div class="col">
+                    Rute
+                  </div>
+                  <div class="col">
+                    Harga
+                  </div>
+                  <div class="col">
+                    Jenis Harga
+                  </div>
+                  <div class="col">
+                    Tonase
+                  </div>
+                  <div class="col">
+                    Harga Tambahan
+                  </div>
+                  <div class="col-sm">
+                    Discount
+                  </div>
+                  <div class="col-sm">
+                    Total
+                  </div>
+                </div>
+              </div>
+
             <div class="col-12">
-                <table class="table table-bordered">
 
-                    <tr style="background-color:  #ec1d24;color: white;">
-
-                        <th scope="col"> <center>Customer</center></th>
-
-                    </tr>
-
-                    <tr>
-
-
-                        <td><div class="col-sm-4">Kode Transaksi</div>
-                        <div class="col-sm-4"><input style="width: 120px;"type='text' enabled></div>
-                    </td>
-                    <td><div class="col-sm-4">Tanggal</div>
-                        <div class="col-sm-4"><input style="width: 120px;"type='text' enabled></div>
-                    </tr>
-
-                    <tr>
-
-
-                        <td><div class="col-sm-4">Admin</div>
-                            <div class="col-sm-4"><input style="width: 120px;"type='text' enabled></div>
-                            <td><div class="col-sm-4">Waktu</div>
-                                <div class="col-sm-4"><input style="width: 120px;"type='text' enabled></div>
-                    </tr>
-
-                    <tr>
-
-
-                        <td><div class="col-sm-4">Subtotal</div>
-                            <div class="col-sm-4"><input style="width: 120px;"type='text' enabled></div>
-                        <td>Rp.</td>
-
-                    </tr>
-
-                    <tr>
-
-
-                        <td><div class="col-sm-4">Diskon</div>
-                            <div class="col-sm-4"><input style="width: 120px;"type='text' enabled></div>
-                        <td>Rp.</td>
-
-                    </tr>
-
-                </table>
-            </div>
-            <button type="button" style="float: right;" name="" class="btn btn-outline-danger">ADD</button>
-
-            <div class="col-12">
-                <table class="table table-bordered">
-
-                    <tr style="background-color:  #ec1d24;color: white;">
-
-                        <th scope="col"> <center>Keranjang</center></th>
-
-                    </tr>
-
-                    <tr>
-
-
-                        <td><div class="col-sm-4">Ukuran</div>
-                        <div class="col-sm-4"><input style="width: 120px;"type='text' enabled></div>
-                    </td>
-                        <td><div class="col-sm-4">Volume</div>
-                        <div class="col-sm-4"><input style="width: 120px;"type='text' enabled></div>
-                    </td>
-                    <td><div class="col-sm-4">Berat</div>
-                        <div class="col-sm-4"><input style="width: 120px;"type='text' enabled></div>
-                    </td>
-                    </tr>
-                </table>
-            </div>
-
-            <div class="col-12">
-                <table class="table table-bordered">
-
-                    <tr style="background-color:  #ec1d24;color: white;">
-
-                        <th scope="col"> <center>Transaksi</center></th>
-
-                    </tr>
-
-                    <tr style="background-color:  white;color: #ec1d24;">
-
-                        <th scope="col"> <center>Nama Customer</center></th>
-                        <th scope="col"> <center>Rute</center></th>
-                        <th scope="col"> <center>Harga</center></th>
-                        <th scope="col"> <center>Jenis Harga</center></th>
-                        <th scope="col"> <center>Tonase</center></th>
-                        <th scope="col"> <center>Harga Tambahan</center></th>
-                        <th scope="col"> <center>Discount</center></th>
-                        <th scope="col"> <center>Total</center></th>
-
-
-                    </tr>
-
-                    <tr>
-
-
-                    <td>
-                    </td>
-                    <td>
-                    </td>
-                    <td>
-                    </td>
-                    <td>
-                    </td>
-                    <td>
-                    </td>
-                    <td>
-                    </td>
-                    <td>
-                    </td>
-                    </tr>
-                </table>
             </div>
         </div>
     </div>
