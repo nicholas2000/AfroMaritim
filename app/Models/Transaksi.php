f0be1e03b070e79e011e93613b41508cdbd1656e
JeffreyAdrian27 <jeffreyadrian27@gmail.com>
2023-02-04 14:38:38 +0700

    model transaksi

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Transaksi extends Model
 {
     use HasFactory;
+    protected $table = "transaksi";
+    protected $primaryKey = "nomor_transaksi";
+    public $timestamps = false;
+    protected $fillable = [
+        'nomor_transaksi',
+        'id_customer',
+        'nama_barang',
+        'id_admin',
+        'ukuran',
+        'volume',
+        'berat',
+        'rute',
+        'harga',
+        'jenis_harga',
+        'tonase',
+        'harga_tambahan',
+        'persentase',
+        'total_harga',
+        'nama_kapal',
+        'nama_container',
+        'tanggal_berangkat'
+    ];
+    protected $keyType = 'string';
 }
