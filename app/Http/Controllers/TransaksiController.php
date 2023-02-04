5d3002f1b190e5a304297507aa96df848c58e3e3
JeffreyAdrian27 <jeffreyadrian27@gmail.com>
2023-02-04 15:16:51 +0700

    transaksi controller bug

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class TransaksiController extends Controller
 {
+    public function showtransaksi(Request $req)
+    {
+        return view('admin/mTransaksi');
+    }
 
+    public function doAdd(Request $req)
+    {
+        dd("albert kampret");
+    }
 }
