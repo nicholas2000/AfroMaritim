432f8b61fd25e9aad69b9384bc3c71642fba1d45
JeffreyAdrian27 <jeffreyadrian27@gmail.com>
2023-02-07 14:08:47 +0700

    hapus url

--- resources/views/admin/mCabang.blade.php
+++ resources/views/admin/mCabang.blade.php
@@ -1,5 +1,4 @@
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <link rel="stylesheet" href="sidebar.css">
 <script src="sidebar.js"></script>
