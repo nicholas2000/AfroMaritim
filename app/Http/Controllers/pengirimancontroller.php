be4e00aa15c8e76a568eb68fc29c0bdfd750d3e1
KevinOwen21 <kevinowen05@gmail.com>
2023-02-14 09:50:15 +0700

    Update pengirimancontroller.php

--- app/Http/Controllers/pengirimancontroller.php
+++ app/Http/Controllers/pengirimancontroller.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\Pengiriman;
 use App\Models\Transaksi;
 use Illuminate\Http\Request;
 
@@ -9,6 +11,7 @@ class pengirimancontroller extends Controller
     public function vmpengirimansatu()
     {
         $param['arrHistory']=Transaksi::get();
+        $param['pengiriman']=Pengiriman::get();
         return view('admin.mOpsi1',$param);
     }
     public function vmpengirimandua()
