43903e13b41a32dda5a65c1b7331c25a9f593c4f
JeffreyAdrian27 <jeffreyadrian27@gmail.com>
2023-02-16 13:02:36 +0700

    autocomplete

--- public/autocomplete.php
+++ public/autocomplete.php
@@ -11,6 +11,24 @@
             $query = "UPDATE permission SET ".$_POST["query1"]. "= 0 WHERE daftar_berita='".$_POST["query2"]."'";
             $result = mysqli_query($connect, $query);
         }
+    }else if($_REQUEST["ctr"]=="Ftrans"){
+        if(isset($_POST["query"])){
+            $output = '';
+            $query = "SELECT * FROM master_tcustomer WHERE nama_customer LIKE '%".$_POST["query"]."%'";
+            $result = mysqli_query($connect, $query);
+            $output = '<ul class="list-unstyled">';
+
+            if(mysqli_num_rows($result) > 0){
+                while($row = mysqli_fetch_array($result)){
+                    $output .= '<li>'.$row["nama_customer"].'</li>';
+                }
+            }else{
+                $output .= '<li>User Not Found</li>';
+            }
+
+        $output .= '</ul>';
+        echo $output;
+        }
     }
     // else if($_REQUEST["ctr"]=="Fhistory"){
     //     if(isset($_POST["query"])){
