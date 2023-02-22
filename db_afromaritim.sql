40371598c5afe4c4a7962c925035643d02e33969
KevinOwen21 <kevinowen05@gmail.com>
2023-02-22 22:05:02 +0700

    db lagi

--- db_afromaritim.sql
+++ db_afromaritim.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 22 Feb 2023 pada 15.58
+-- Waktu pembuatan: 22 Feb 2023 pada 16.04
 -- Versi server: 10.4.25-MariaDB
 -- Versi PHP: 8.1.10
 
