34f3978266d36761eb9b5c0c3b38e1d4cd91deb4
JeffreyAdrian27 <jeffreyadrian27@gmail.com>
2023-03-14 11:01:02 +0700

    db baru

--- db_afromaritim.sql
+++ db_afromaritim.sql
@@ -30,19 +30,30 @@ CREATE TABLE `activity` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `activity` */
 
-insert  into `activity`(`id`,`user`,`activity`,`ip_address`,`browser`,`os`,`created_at`,`updated_at`) values
-(1,'Nicho','Login Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-02 07:35:45','2023-03-02 07:35:45'),
-(2,'Nicho','Login Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-06 04:42:36','2023-03-06 04:42:36'),
-(3,'Nicho','Login Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-07 04:33:36','2023-03-07 04:33:36'),
-(4,'Nicho','Login Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-07 12:26:25','2023-03-07 12:26:25'),
-(5,'Nicho','Login Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-08 03:06:09','2023-03-08 03:06:09'),
-(6,'Nicho','Login Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-09 03:07:45','2023-03-09 03:07:45'),
-(7,'Nicho','Login Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-09 03:51:25','2023-03-09 03:51:25'),
-(8,'Nicho','Login Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-14 03:02:48','2023-03-14 03:02:48');
+insert  into `activity`(`id`,`user`,`activity`,`ip_address`,`browser`,`os`,`created_at`,`updated_at`) values 
+(35,'Nicho','Login Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-09 11:30:02','2023-03-09 11:30:02'),
+(36,'Nicho','Login Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-09 11:31:40','2023-03-09 11:31:40'),
+(37,'Nicho','Logout Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-09 11:31:44','2023-03-09 11:31:44'),
+(38,'Nicho','Login Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-09 11:53:31','2023-03-09 11:53:31'),
+(39,'Nicho','Logout Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-09 12:02:26','2023-03-09 12:02:26'),
+(40,'Andi','Login Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-09 12:02:32','2023-03-09 12:02:32'),
+(41,'Andi','Login Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-14 03:02:03','2023-03-14 03:02:03'),
+(42,'Andi','Logout Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-14 03:02:33','2023-03-14 03:02:33'),
+(43,'Andi','Login Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-14 03:02:48','2023-03-14 03:02:48'),
+(44,'Andi','Logout Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-14 03:03:03','2023-03-14 03:03:03'),
+(45,'Andi','Login Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-14 03:03:51','2023-03-14 03:03:51'),
+(46,'Andi','Logout Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-14 03:10:50','2023-03-14 03:10:50'),
+(47,'Nicho','Login Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-14 03:10:54','2023-03-14 03:10:54'),
+(48,'Nicho','Logout Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-14 03:11:32','2023-03-14 03:11:32'),
+(49,'Andi','Login Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-14 03:11:40','2023-03-14 03:11:40'),
+(50,'Andi','Logout Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-14 03:12:09','2023-03-14 03:12:09'),
+(51,'Nicho','Login Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-14 03:12:17','2023-03-14 03:12:17'),
+(52,'Nicho','Logout Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-14 03:55:42','2023-03-14 03:55:42'),
+(53,'Budi','Login Berhasil','127.0.0.1','Chrome','Windows 10','2023-03-14 03:55:55','2023-03-14 03:55:55');
 
 /*Table structure for table `depo` */
 
@@ -103,8 +114,9 @@ CREATE TABLE `master_tcabang` (
 
 /*Data for the table `master_tcabang` */
 
-insert  into `master_tcabang`(`id_cabang`,`nama_cabang`,`jum_cabang`,`alamat_cabang`,`provinsi_cabang`,`kota_cabang`,`kodepos_cabang`,`telpon_cabang`,`gmaps_cabang`,`email_cabang`,`status_cabang`,`created_at`,`updated_at`,`deleted_at`) values
-('C001','CabangSurabaya','4','Ngagel Jaya no 3','Jawa Barat','Bandung','61213','08113190080','https://afrotransmaritim.co.id/','admin@gmail.com','1','2023-02-13 08:55:02','2023-02-14 00:37:41',NULL);
+insert  into `master_tcabang`(`id_cabang`,`nama_cabang`,`jum_cabang`,`alamat_cabang`,`provinsi_cabang`,`kota_cabang`,`kodepos_cabang`,`telpon_cabang`,`gmaps_cabang`,`email_cabang`,`status_cabang`,`created_at`,`updated_at`,`deleted_at`) values 
+('C001','CabangMakassar','4','Jl Makassar no 1','Sulawesi Selatan','Makassar','61213','08113190080','https://afrotransmaritim.co.id/','cabang1@gmail.com','1','2023-02-13 08:55:02','2023-02-14 00:37:41',NULL),
+('C002','CabangSurabaya','4','Jl Surabaya no 1','Jawa Timur','Surabaya','61213','08113190080','https://afrotransmaritim.co.id/','cabang2@gmail.com','1','2023-02-13 08:55:02','2023-02-14 00:37:41',NULL);
 
 /*Table structure for table `master_tcontainer` */
 
@@ -122,9 +134,9 @@ CREATE TABLE `master_tcontainer` (
 
 /*Data for the table `master_tcontainer` */
 
-insert  into `master_tcontainer`(`nomor_container`,`nama_container`,`status`,`created_at`,`updated_at`,`deleted_at`) values
+insert  into `master_tcontainer`(`nomor_container`,`nama_container`,`status`,`created_at`,`updated_at`,`deleted_at`) values 
 ('CO001','A','1','2023-03-02 07:52:07','2023-03-06 08:58:03',NULL),
-('CO002','B','0','2023-03-02 07:52:07','2023-03-02 07:52:07',NULL);
+('CO002','B','1','2023-03-02 07:52:07','2023-03-02 07:52:07',NULL);
 
 /*Table structure for table `master_tcustomer` */
 
@@ -156,7 +168,7 @@ CREATE TABLE `master_tcustomer` (
 
 /*Data for the table `master_tcustomer` */
 
-insert  into `master_tcustomer`(`id_customer`,`id_cabang`,`nama_customer`,`npwp`,`jalan`,`provinsi`,`kode_pos`,`telpon`,`email`,`pajak`,`status_hutang`,`total_hutang`,`batas_pembayaran`,`no_rekening`,`metode_pembayaran`,`created_at`,`updated_at`,`deleted_at`) values
+insert  into `master_tcustomer`(`id_customer`,`id_cabang`,`nama_customer`,`npwp`,`jalan`,`provinsi`,`kode_pos`,`telpon`,`email`,`pajak`,`status_hutang`,`total_hutang`,`batas_pembayaran`,`no_rekening`,`metode_pembayaran`,`created_at`,`updated_at`,`deleted_at`) values 
 ('CU001','C001','Jeffrey','923217421','jalan ngagel jaya tengah no 73-77','Jawa Barat','63432','028349328042390','cabangsba@gmail.com','Tidak Kena Pajak','Off','','','','','2023-03-06 08:58:46','2023-03-06 08:58:46',NULL);
 
 /*Table structure for table `master_tjenis` */
@@ -174,7 +186,7 @@ CREATE TABLE `master_tjenis` (
 
 /*Data for the table `master_tjenis` */
 
-insert  into `master_tjenis`(`tipe`,`jenis_harga`,`nominal`,`created_at`,`updated_at`) values
+insert  into `master_tjenis`(`tipe`,`jenis_harga`,`nominal`,`created_at`,`updated_at`) values 
 ('A','Berat','1000',NULL,NULL);
 
 /*Table structure for table `master_tkompetitor` */
@@ -209,13 +221,14 @@ CREATE TABLE `master_tpegawai` (
   `id_pegawai` varchar(255) NOT NULL,
   `id_cabang` varchar(255) NOT NULL,
   `nama_pegawai` varchar(255) NOT NULL,
-  `npwp_pegawai` varchar(255) NOT NULL,
+  `npwp_pegawai` varchar(255) DEFAULT NULL,
   `alamat_pegawai` varchar(255) NOT NULL,
-  `provinsi_pegawai` varchar(255) NOT NULL,
-  `kota_pegawai` varchar(255) NOT NULL,
-  `kodepos_pegawai` varchar(255) NOT NULL,
+  `provinsi_pegawai` varchar(255) DEFAULT NULL,
+  `kota_pegawai` varchar(255) DEFAULT NULL,
+  `kodepos_pegawai` varchar(255) DEFAULT NULL,
   `telp_pegawai` varchar(255) NOT NULL,
   `email_pegawai` varchar(255) NOT NULL,
+  `password_pegawai` varchar(255) NOT NULL,
   `role_pegawai` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
@@ -227,8 +240,10 @@ CREATE TABLE `master_tpegawai` (
 
 /*Data for the table `master_tpegawai` */
 
-insert  into `master_tpegawai`(`id_pegawai`,`id_cabang`,`nama_pegawai`,`npwp_pegawai`,`alamat_pegawai`,`provinsi_pegawai`,`kota_pegawai`,`kodepos_pegawai`,`telp_pegawai`,`email_pegawai`,`role_pegawai`,`created_at`,`updated_at`,`deleted_at`) values
-('P001','C001','Nicho','23423','Kabupaten Cikalang no 5','Jawa Timur','Surabaya','3334534','08113190080','admin@gmail.com','Super Admin','2023-02-13 08:55:32','2023-02-13 08:55:32',NULL);
+insert  into `master_tpegawai`(`id_pegawai`,`id_cabang`,`nama_pegawai`,`npwp_pegawai`,`alamat_pegawai`,`provinsi_pegawai`,`kota_pegawai`,`kodepos_pegawai`,`telp_pegawai`,`email_pegawai`,`password_pegawai`,`role_pegawai`,`created_at`,`updated_at`,`deleted_at`) values 
+('P001','C001','Nicho','23423','Kabupaten Cikalang no 5','Sulawesi Selatan','Makassar','3334534','08113190080','superadmin@gmail.com','$2y$10$Zp0lqCGIVRvCnG04gOfQjeNFOP7dYHcqXXSt2qx0Se4JvT58sek2S','Super Admin','2023-03-09 11:18:58','2023-03-09 11:18:58',NULL),
+('P002','C001','Andi','231312','Jln. Ngagel Jaya no 4','Jawa Timur','Surabaya','231233','08113190080','admin2@gmail.com','$2y$10$u2kFoMtHRiPAjDip5XOS.OL80e4d6R2hFXUBS3rih2m5ft29gRGUm','Admin Surabaya','2023-03-09 11:26:09','2023-03-09 11:26:09',NULL),
+('P003','C001','Budi',NULL,'Jln Makassar no  3','Sulawesi Selatan',NULL,NULL,'08113190080','admin3@gmail.com','$2y$10$4ePJhFYMr6HL/7S8MCuZ2OC9LKbJ2wRcz2cSspdep9v5yz.8cFrQ.','Admin Makassar','2023-03-14 03:54:39','2023-03-14 03:54:39',NULL);
 
 /*Table structure for table `migrations` */
 
@@ -243,7 +258,7 @@ CREATE TABLE `migrations` (
 
 /*Data for the table `migrations` */
 
-insert  into `migrations`(`id`,`migration`,`batch`) values
+insert  into `migrations`(`id`,`migration`,`batch`) values 
 (1,'2014_10_12_000000_create_users_table',1),
 (2,'2014_10_12_100000_create_password_resets_table',1),
 (3,'2019_08_19_000000_create_failed_jobs_table',1),
@@ -286,22 +301,29 @@ DROP TABLE IF EXISTS `permission`;
 CREATE TABLE `permission` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `daftar_berita` varchar(255) DEFAULT NULL,
-  `admin` tinyint(1) DEFAULT NULL,
+  `adminmakassar` tinyint(1) DEFAULT NULL,
+  `adminsurabaya` tinyint(1) DEFAULT NULL,
   `accounting` tinyint(1) DEFAULT NULL,
   `kurir` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `permission` */
 
-insert  into `permission`(`id`,`daftar_berita`,`admin`,`accounting`,`kurir`) values
-(1,'Master Cabang',1,0,0),
-(2,'Master Pegawai',0,0,0),
-(3,'Master Customer',0,0,0),
-(4,'Master Kompetitor',0,0,0),
-(5,'Master Jenis Harga',0,0,0),
-(6,'Transaksi',0,0,0),
-(7,'History Transaksi',0,0,0);
+insert  into `permission`(`id`,`daftar_berita`,`adminmakassar`,`adminsurabaya`,`accounting`,`kurir`) values 
+(1,'Master Cabang',0,0,0,0),
+(2,'Master Pegawai',0,0,0,0),
+(3,'Master Customer',0,0,0,0),
+(4,'Master Kompetitor',0,0,0,0),
+(5,'Master Jenis Harga',0,0,0,0),
+(6,'Transaksi',0,0,0,0),
+(7,'History Transaksi',0,0,0,0),
+(8,'Master Container',0,0,0,0),
+(9,'Depo',0,0,0,0),
+(10,'Status Barang',0,0,0,0),
+(11,'Master Team Pengiriman',0,0,0,0),
+(12,'Gudang',0,0,0,0),
+(13,'Master Piutang',0,0,0,0);
 
 /*Table structure for table `personal_access_tokens` */
 
@@ -354,8 +376,8 @@ CREATE TABLE `transaksi` (
   `total_harga` varchar(255) DEFAULT NULL,
   `tanggal` date DEFAULT NULL,
   `nomor_container` varchar(255) DEFAULT NULL,
-  `catatan` varchar(255) DEFAULT NULL,
   `link_foto` varchar(255) DEFAULT NULL,
+  `catatan` varchar(255) DEFAULT NULL,
   `status_barang` varchar(255) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
@@ -367,11 +389,10 @@ CREATE TABLE `transaksi` (
 
 /*Data for the table `transaksi` */
 
-insert  into `transaksi`(`nomor_manifest`,`nomor_resi`,`nomor_segel`,`nama_pengirim`,`alamat_pengirim`,`nohp_pengirim`,`email_pengirim`,`nama_penerima`,`alamat_penerima`,`nohp_penerima`,`email_penerima`,`jenis_barang`,`jumlah_barang`,`jenis_volume`,`volume`,`jenis_harga`,`rute`,`nama_kapal`,`harga_kubik`,`harga`,`harga_tambahan`,`harga_potongan`,`total_harga`,`tanggal`,`nomor_container`,`catatan`,`link_foto`,`status_barang`,`created_at`,`updated_at`,`deleted_at`) values
-(NULL,'1',NULL,'aston',NULL,NULL,NULL,'aston',NULL,NULL,NULL,'Gula','23',NULL,'2m',NULL,NULL,NULL,'0','0','0','0','0','2023-03-07','CO001',NULL,NULL,'Depo','2023-03-07 04:15:37','2023-03-07 04:15:37',NULL),
-(NULL,'1123123',NULL,'leorio',NULL,NULL,NULL,'leorio',NULL,NULL,NULL,'Gula','23',NULL,'2',NULL,NULL,NULL,'0','0','0','0','0','2023-03-14',NULL,'ini dilempar',NULL,'Depo','2023-03-14 03:22:21','2023-03-14 03:22:21',NULL),
-(NULL,'112323213222',NULL,'leorio',NULL,NULL,NULL,'leorio',NULL,NULL,NULL,'Gula','23',NULL,'2',NULL,NULL,NULL,'0','0','0','0','0','2023-03-14',NULL,'ini dilempar',NULL,'Depo','2023-03-14 03:23:10','2023-03-14 03:23:10',NULL),
-(NULL,'32323','44','Rudi','Surabaya','3434343','lala@gmail.com','Ayam','Surabaya','43434343','lala@gmail.com',NULL,'','Berat',NULL,'A,Berat,1000','Makassar','Poseidon','44444','16789743212','3434343','434343','16792743212','2023-03-15',NULL,NULL,NULL,'Kantor SBY','2023-03-02 08:29:23','2023-03-02 08:30:52',NULL);
+insert  into `transaksi`(`nomor_manifest`,`nomor_resi`,`nomor_segel`,`nama_pengirim`,`alamat_pengirim`,`nohp_pengirim`,`email_pengirim`,`nama_penerima`,`alamat_penerima`,`nohp_penerima`,`email_penerima`,`jenis_barang`,`jumlah_barang`,`jenis_volume`,`volume`,`jenis_harga`,`rute`,`nama_kapal`,`harga_kubik`,`harga`,`harga_tambahan`,`harga_potongan`,`total_harga`,`tanggal`,`nomor_container`,`link_foto`,`catatan`,`status_barang`,`created_at`,`updated_at`,`deleted_at`) values 
+(NULL,'1',NULL,'Kevin',NULL,NULL,NULL,'Jeffrey',NULL,NULL,NULL,'Daging','4',NULL,'10',NULL,NULL,NULL,'0','0','0','0','0','2023-03-07',NULL,NULL,NULL,'Depo','2023-03-07 16:20:31','2023-03-07 16:20:31',NULL),
+(NULL,'32323','44','Rudi','Surabaya','3434343','lala@gmail.com','Ayam','Surabaya','43434343','lala@gmail.com',NULL,'','Berat',NULL,'A,Berat,1000','Makassar','Poseidon','44444','16789743212','3434343','434343','16792743212','2023-03-15',NULL,NULL,NULL,'Kantor SBY','2023-03-02 08:29:23','2023-03-02 08:30:52',NULL),
+(NULL,'5',NULL,'Jeffrey',NULL,NULL,NULL,'Nicho',NULL,NULL,NULL,'Mie','4',NULL,'10',NULL,NULL,NULL,'0','0','0','0','0','2023-03-07','CO001',NULL,NULL,'Depo','2023-03-07 16:21:23','2023-03-07 16:21:23',NULL);
 
 /*Table structure for table `users` */
 
