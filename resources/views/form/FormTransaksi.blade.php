ea095c7d39f717b806827172d1071e1b959c1030
NicholasTj <64400543+NicholasTj@users.noreply.github.com>
2023-03-29 00:01:50 +0700

    Update FormTransaksi.blade.php

--- resources/views/form/FormTransaksi.blade.php
+++ resources/views/form/FormTransaksi.blade.php
@@ -141,13 +141,10 @@
                         <div class="col-sm-1">
                             Ukuran :
                         </div>
-<<<<<<< HEAD
                         <div class="col-sm">
                             <input name="ukuran" class="form-control" type="text"style="width: 210px;" >
-=======
                         <div class="col-sm-2">
                             <input type="checkbox" name="option-1" id="option-1" onclick="disable_option1()">
->>>>>>> f411128c848bf22a3d2956dc3164b912977943ea
                         </div>
                         <div class="col-sm-3">
                             <input name="ukuran" id="ukuran" class="form-control" type="text"style="width: 210px;" >
